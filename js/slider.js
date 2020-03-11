12fc72c06f0a305cffd502e51e7e34d88c71e7ba
dima xmiel <dima.hmelios@gmail.com>
2020-03-12 01:09:03 +0300

    feat: add background slides and screen off

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,25 @@
-window.onload = function() {
-  const sliderContainer = document.querySelector('.slider');
+window.onload = function () {
+  const sliderContainer = document.querySelector( '.slider' );
 
-  sliderContainer.addEventListener('click', e => {
+  sliderContainer.addEventListener( 'click', e => {
     e.stopPropagation();
-    if (e.target.tagName === 'BUTTON') {
-      switch (true) {
+    if ( e.target.tagName === 'BUTTON' ) {
+      switch ( true ) {
         case e.target.dataset.direction == 'left':
-          slider.slide('left');
+          slider.slide( 'left' );
           break;
         case e.target.dataset.direction == 'right':
-          slider.slide('right');
+          slider.slide( 'right' );
           break;
         default:
       }
     }
-  });
+  } );
 
-  document.addEventListener('keyup', e => {
-    if (e.keyCode === 37) slider.slide('left');
-    else if (e.keyCode === 39) slider.slide('right');
-  });
+  document.addEventListener( 'keyup', e => {
+    if ( e.keyCode === 37 ) slider.slide( 'left' );
+    else if ( e.keyCode === 39 ) slider.slide( 'right' );
+  } );
 
   const slider = new Slider();
   slider.getImages();
@@ -27,46 +27,52 @@ window.onload = function() {
 
 class Slider {
   constructor() {
+    this.isScreenOn = false;
     this.images = [];
+    this.slides = [];
     this.index = null;
     this.isAnimation = false;
   }
 
-  setActive() {
-    this.images.map((e, i) => {
+  setActive () {
+    this.images.map( ( e, i ) => {
       if (
-        e.classList.contains('phone-screen__image-verical') &&
-        e.classList.contains('active')
+        e.classList.contains( 'phone-screen__image-verical' ) &&
+        e.classList.contains( 'active' )
       )
         this.index = i;
-    });
+    } );
   }
 
-  removeAllClases = ({ target }) => {
-    this.images.map(e => {
-      e.classList.remove(
+  removeAllClases = ( { target } ) => {
+    function removeActiveClasses ( el ) {
+      el.classList.remove(
         'move_right',
         'move_left',
         'move_from_right',
         'move_from_left'
       );
-    });
-    target.classList.remove('active');
-    target.removeEventListener('animationend', this.removeAllClases);
+    }
+    this.images.map( removeActiveClasses );
+    this.slides.map( removeActiveClasses );
+
+    target.classList.remove( 'active' );
+    target.removeEventListener( 'animationend', this.removeAllClases );
     this.isAnimation = false;
   };
 
-  slide(animIn) {
-    if (this.isAnimation && !this.index >= 0) return;
+  slide ( animIn ) {
+    this.isScreenOn && this.setActive();
+    if ( this.isAnimation && !this.index >= 0 ) return;
 
     const animOut = animIn == 'right' ? 'left' : 'right';
 
-    if (['right', 'left'].includes(animIn)) {
+    if ( ['right', 'left'].includes( animIn ) ) {
       this.isAnimation = true;
       let nextV = null;
       let nextH = null;
-      const mid = Math.floor(this.images.length / 2);
-      switch (animIn) {
+      const mid = Math.floor( this.images.length / 2 );
+      switch ( animIn ) {
         case 'right':
           nextV = this.index + 1 < mid ? this.index + 1 : 0;
           nextH = nextV + mid;
@@ -76,23 +82,33 @@ class Slider {
           nextH = nextV + mid;
       }
 
-      this.images.filter(slide => {
-        if (slide.classList.contains('active')) {
-          slide.classList.add('move_' + animOut);
-          slide.addEventListener('animationend', this.removeAllClases);
+      const animateItem = ( slide ) => {
+        if ( slide.classList.contains( 'active' ) ) {
+          slide.classList.add( 'move_' + animOut );
+          slide.addEventListener( 'animationend', this.removeAllClases );
         }
-      });
+      }
 
-      this.images[nextV].classList.toggle('active');
-      this.images[nextV].classList.toggle('move_from_' + animIn);
-      this.images[nextH].classList.toggle('active');
-      this.images[nextH].classList.toggle('move_from_' + animIn);
+      if(this.isScreenOn) {
+        this.images.forEach( animateItem );
+        this.images[nextV].classList.toggle( 'active' );
+        this.images[nextV].classList.toggle( 'move_from_' + animIn );
+        this.images[nextH].classList.toggle( 'active' );
+        this.images[nextH].classList.toggle( 'move_from_' + animIn );
+
+      }
+
+      this.slides.forEach( animateItem );
+
+      this.slides[nextV].classList.toggle( 'active' );
+      this.slides[nextV].classList.toggle( 'move_from_' + animIn );
       this.index = nextV;
     }
   }
 
-  getImages() {
-    this.images = [...document.querySelectorAll('[data-image]')];
+  getImages () {
+    this.images = [...document.querySelectorAll( '[data-image]' )];
+    this.slides = [...document.querySelectorAll( '[data-slide]' )]
     this.setActive();
   }
 }
